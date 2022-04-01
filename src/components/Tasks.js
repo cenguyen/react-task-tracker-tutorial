cd21ae3c8ffb3e22022b905520337f4db182986f
cenguyen <37422560+cenguyen@users.noreply.github.com>
2022-04-01 14:27:36 -0500

    added doubleClick for reminder function, added deleteTask function

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,15 @@
-// const tasks = [
-//   {
-//     id: 1,
-//     text: 'Doctors Appointment',
-//     day: 'Feb 5th at 10:30AM',
-//     reminder: true,
-//   },
-//   {
-//     id: 2,
-//     text: 'Meeting at School',
-//     day: 'Feb 6th at 1:30PM',
-//     reminder: true,
-//   },
-//   {
-//     id: 3,
-//     text: 'Grocery shopping',
-//     day: 'Feb 5th at 2:30PM',
-//     reminder: false,
-//   },
-// ];
+import Task from './Task';
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
